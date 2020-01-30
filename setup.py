5b20240bea85f5219edbef53c4e3ca23d0f5a672
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2020-01-30 18:47:54 +0900

    Package project as fbjson2table.
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fbjson2table", # Replace with your own username
+    version="1.0.0",
+    author="Numbers",
+    author_email="hi@numbersprotocol.io",
+    description="Parse Facebook archive JSON files to tables",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/numbersprotocol/fb-json2table",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
