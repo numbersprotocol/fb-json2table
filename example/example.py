98a9515cdd46413b236d04face6540ccdc7ee3a3
george <george@gc2.dt42.io>
2020-02-03 15:54:52 +0800

    modify example

--- example/example.py
+++ example/example.py
@@ -1,34 +1,9 @@
 import fbjson2table
 from tabulate import tabulate
 from fbjson2table.table_class import TempDFs
+from fbjson2table.func_lib import parse_fb_json
 
-json_content = [ {
-    "timestamp": 1563801255,
-    "attachments": [],
-    "data": [{"update_timestamp": 1563801255}],
-    "title": "Good morning"
-  },
-  {
-    "timestamp": 1543418957,
-    "attachments": [],
-    "data": [{"update_timestamp": 1543418957}],
-    "title": "Good evening"
-  },
-  {
-    "timestamp": 1528528001,
-    "attachments": [
-                        {"data": [
-                           {"media": {"uri": "photos_and_videos/your_posts/fine.jpg",
-                                                 "creation_timestamp": 1528527958,
-                                           "media_metadata": {"photo_metadata": {"upload_ip": "31.2.7.58"}},
-                                           "title": "",
-                                           "description": "I am fine"}}]}
-    ],
-    "data": [{"post": "hey"}],
-    "title": "thank you"
- }
-]
-
+json_content = parse_fb_json("./example_json_content.json")
 temp_dfs = TempDFs(json_content)
 
 for df, table_name in zip(temp_dfs.df_list, temp_dfs.table_name_list):
